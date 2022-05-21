3b50ebf4bf1f952705203961eda3c297634cf08d
Kaden Hansen <kadendeanhansen@gmail.com>
2022-05-21 11:19:31 -0400

    add function to read the order of eating lunch

--- python_practice.py
+++ python_practice.py
@@ -5,4 +5,15 @@ hello()
 def pack(item1, item2, item3):
     list = [item1, item2, item3]
     return list
-pack("sandwich", "apple", "pudding")
+print(pack("sandwich", "apple", "pudding"))
+
+def eat_lunch(*items):
+    if items:
+        for i in items:
+            if i == items[0]:
+                print(f"First I eat my {i}")
+            else:
+                print(f"Next I eat my {i}")
+    else:
+        print("My lunchbox is empty!")
+eat_lunch('sandwich', "apple", "pudding")
